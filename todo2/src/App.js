a4a7f23e462018023c02179697b09d641a7eaf9d
marianefreitas <mariane11.freitas@gmail.com>
2023-01-04 21:34:23 -0300

    iniciando a criação da aplicacao de lista de tarefas

---
+++ todo2/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
+
+function App() {
+
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="App">
+
+      <div className='todo-header'>
+        <h1>React TO DO</h1>
+      </div>
+
+      <div className='form-todo'>
+        <p>Formulário</p>
+      </div>
+
+      <div className='list-todo'>
+        <h2>Lista de tarefas</h2>
+        { todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;

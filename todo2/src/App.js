68d5cefb706a2d725b7ef88b56a235990c3df60c
marianefreitas <mariane11.freitas@gmail.com>
2023-01-06 14:42:24 -0300

    criando e estilizando as divs do formulario

--- todo2/src/App.js
+++ todo2/src/App.js
@@ -11,6 +11,12 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => {
+    {/* preventDefault serve para quando o formulário for enviado a página não seja recarregada(SPA) */}
+    e.preventDefault();
+
+  }
+
   return (
     <div className="App">
 
@@ -19,7 +25,36 @@ function App() {
       </div>
 
       <div className='form-todo'>
-        <p>Formulário</p>
+        <h2>Insira a próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+
+          <div className='form-control'>
+            <label htmlFor='title'>O que você vai fazer?</label>
+            <input 
+            type='text' 
+            name='title' 
+            placeholder='Título da tarefa' 
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title || ""}
+            required
+            />
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor='time'>Duração:</label>
+            <input
+            type='text'
+            name='time'
+            placeholder='Tempo estimado (em horas)'
+            onChange={(e) => { setTime(e.target.value)}}
+            value={time || ""}
+            required
+            />
+          </div>
+          
+          <input type='submit' value='Criar Tarefa'/>
+
+        </form>
       </div>
 
       <div className='list-todo'>

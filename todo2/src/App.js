146267cc1b8a3aca95345f84d37f97dab6a0ef30
marianefreitas <mariane11.freitas@gmail.com>
2023-01-16 14:17:53 -0300

    adicionando comunicações com servidor usando fetch

--- todo2/src/App.js
+++ todo2/src/App.js
@@ -11,27 +11,78 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    {/* preventDefault serve para quando o formulário for enviado a página não seja recarregada(SPA) */}
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+      setTodos(res);
+    }
+
+    loadData();
+
+  }, [])
+
+  const handleSubmit = async (e) => {
+    {/* preventDefault serve para quando o formulário for enviado a página não seja recarregada(SPA) */ }
     e.preventDefault();
 
     const todo = {
-      id: Math.random(),
+      id: Math.floor(Date.now() * Math.random()),
       title,
       time,
       done: false
     }
 
     // envio api
-    console.log(todo);
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo])
 
     setTitle("");
     setTime("");
 
   }
 
+  const handleDete = async (id) => {
+    await fetch(API + "/todos/" + id , {
+      method: "DELETE"
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id))
+  }
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "Application/json"
+      }
+    })
+
+    setTodos((prevState)=> prevState.map((t) => (t.id === data.id ? t=data : t)))
+  }
+
+  if (loading) {
+    <p> Carregando...</p>
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
 
       <div className='todo-header'>
         <h1>React TO DO</h1>
@@ -43,36 +94,48 @@ function App() {
 
           <div className='form-control'>
             <label htmlFor='title'>O que você vai fazer?</label>
-            <input 
-            type='text' 
-            name='title' 
-            placeholder='Título da tarefa' 
-            onChange={(e) => setTitle(e.target.value)} 
-            value={title || ""}
-            required
+            <input
+              type='text'
+              name='title'
+              placeholder='Título da tarefa'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
             />
           </div>
 
           <div className='form-control'>
             <label htmlFor='time'>Duração:</label>
             <input
-            type='text'
-            name='time'
-            placeholder='Tempo estimado (em horas)'
-            onChange={(e) => { setTime(e.target.value)}}
-            value={time || ""}
-            required
+              type='text'
+              name='time'
+              placeholder='Tempo estimado (em horas)'
+              onChange={(e) => { setTime(e.target.value) }}
+              value={time || ""}
+              required
             />
           </div>
 
-          <input type='submit' value='Criar Tarefa'/>
+          <input type='submit' value='Criar Tarefa' />
 
         </form>
       </div>
 
       <div className='list-todo'>
         <h2>Lista de tarefas</h2>
-        { todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) => (
+          <div className='todo' key={todo.id}>
+            <h3 className={todo.done ? 'todo-done' : ''}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className='actions'>
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
 
     </div>
